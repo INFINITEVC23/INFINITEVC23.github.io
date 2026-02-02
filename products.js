a3d16453c5e405058e44cd93548fc01ecead45b2
INFINITEVC23 <andreamateo1990@gmail.com>
2026-02-02 20:40:30 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,8 +1,4 @@
-// Base64 encoded JSON of products
-// Admin edits this file and commits to update shop
-const productsBase64 = "W3sibmFtZSI6IkRpYW1vbmQgU3RhY2siLCJwcmljZSI6NjQsImltYWdlIjoiaHR0cHM6Ly92aWEucGxhY2Vob2xkZXIuY29tLzE1MCIifSx7Im5hbWUiOiJHb2xkIEluZ290IiwicHJpY2UiOjMyLCJpbWFnZSI6Imh0dHBzOi8vdmlhLnBsYWNlaG9sZGVyLmNvbS8xNTAifV0=";
-
-// Function to decode products
-function getProducts(){
-  return JSON.parse(atob(productsBase64));
-}
+// Simple array of products
+const products = [
+  {name: "Netherite Upgrade Trims (Paid with diamonds)", price: 64, image: "https://via.placeholder.com/100"},
+];
